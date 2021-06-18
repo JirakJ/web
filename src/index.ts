abface4bd774b458cf9b3381c9c3928a61f6fc2d
Ing. Jakub Jirák <jakub@jakubjirak.com>
2021-06-18 09:33:05 +0200

    Add example of collection usage

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,24 @@
+import 'semantic-ui-css/semantic.min.css';
+import {UserList} from "./views/UserList";
+import {Collection} from "./models/Collection";
+import {IUserProps} from "./interfaces/IUserProps";
 import {User} from "./models/User";
 
-const user = User.buildUser({id: 1})
+const users = new Collection(
+    'http://localhost:3000/users',
+    (json: IUserProps) => {
+        return User.buildUser(json)
+    }
+);
 
+users.on('change', () => {
+    const root = document.getElementById('root');
+    console.log(root)
+    if(root){
+        new UserList(root, users);
+    } else {
+        throw new Error('Root element not found!');
+    }
+});
 
-user.fetch();
-
-user.on('change', () => { console.log(user)})
-
-
+users.fetch();
